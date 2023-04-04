dfe1174204b189fa8e4544f9c1e6362f5a3ceae4
ItzTutu <104566356+tutupro@users.noreply.github.com>
2023-04-04 10:52:21 -0300

    Update Atividade_5.go

--- Atividade_5.go
+++ Atividade_5.go
@@ -1,3 +1,5 @@
+1º atividade:
+
 package main
 
 import (
@@ -19,3 +21,67 @@ func main() {
 
 	fmt.Printf("A palavra que você digitou %s foi convertida para maiusculo, dando: %s", x, o)
 }
+
+2º Atividade:
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// Escreva um programa que receba uma string
+// e remova todos os espaços em branco.
+// Informe ao usuário o resultado.
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Digite uma palavra: ")
+	scanner.Scan()
+	p := scanner.Text()
+
+	o := strings.ReplaceAll(p, " ", "")
+
+	fmt.Printf("A palavra que você digitou %s foi retirada os espaços, dando: %s", p, o)
+}
+
+3º Atividade:
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// Escreva um programa que receba uma string e substitua todas as
+//ocorrências desse caractere na string por outro caractere especificado
+//pelo usuário. Informe ao usuário o resultado.
+
+func main() {
+
+	var i, j string // Criação das variaveis em string
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Digite uma palavra: ")
+	scanner.Scan()
+	p := scanner.Text()
+
+	fmt.Print("Digite a letra que queira substituir: ")
+	fmt.Scan(&i)
+
+	fmt.Print("Digite a letra para substituir a letra que você queira substituir: ")
+	fmt.Scan(&j)
+	o := strings.ReplaceAll(p, i, j)
+
+	fmt.Printf("A palavra que você digitou %s foi retirada os espaços, dando: %s", p, o)
+}
+
+4º Atividade:
